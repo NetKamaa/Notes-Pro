51b5e75ed281910a37747afa1edccc2b8ed6a58a
Tenderly__ <olevnao31@gmail.com>
2026-03-16 19:42:41 +0300

    refactor: elements.js

--- src/elements.js
+++ src/elements.js
@@ -1,13 +1,7 @@
-const form = document.querySelector(".form-ForNewNote");
-const titleInput = document.querySelector(".title");
-const textInput = document.querySelector(".textarea-Text");
-const error = document.querySelector(".error");
-const notesList = document.querySelector(".notes-list");
-
 export const elements = {
-  form,
-  titleInput,
-  textInput,
-  error,
-  notesList,
+  form: document.querySelector(".form-ForNewNote"),
+  titleInput: document.querySelector(".title"),
+  textInput: document.querySelector(".textarea-Text"),
+  error: document.querySelector(".error"),
+  notesList: document.querySelector(".notes-list"),
 };
