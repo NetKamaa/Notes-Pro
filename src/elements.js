bc4a3d78ebf113d110337e903d4e344cc4614df5
Tenderly__ <olevnao31@gmail.com>
2026-03-20 21:14:37 +0300

    docs: add README with project description and screenshots

--- src/elements.js
+++ src/elements.js
@@ -1,7 +1,7 @@
 export const elements = {
   form: document.querySelector(".form-ForNewNote"),
   titleInput: document.querySelector("#title"),
-  textInput: document.querySelector(".textarea-Text"),
+  textInput: document.querySelector("#textarea-Text"),
   error: document.querySelector(".error"),
   notesList: document.querySelector(".notes-list"),
   formSortQuery: document.querySelector(".form-ForSortQuery"),
